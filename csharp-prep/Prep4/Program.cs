03ba789522e332ae24336e1e9d609fdb4946ead6
dmendenhall <113963164+dmendenhall@users.noreply.github.com>
2023-01-11 21:25:09 -0700

    Part 1 & 2 Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new List<int>();
+
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter a number to add to the list or enter 0 to quit: ");
+
+            string number = Console.ReadLine();
+            userInput = int.Parse(number);
+
+            if (userInput != 0)
+            {
+                numberList.Add(userInput);
+            }
+        }
+
+        int sum = 0;
+        foreach (int input in numberList)
+        {
+            sum += input;
+        }
+
+        Console.WriteLine($"The sum is {sum}.");
+
+        float average = ((float)sum) / numberList.Count;
+        Console.WriteLine($"The average is {average}.");
     }
 }
