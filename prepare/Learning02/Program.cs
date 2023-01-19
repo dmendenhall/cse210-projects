42b93e61f6c808d61e368c05a3f87990d29d61a3
dmendenhall <113963164+dmendenhall@users.noreply.github.com>
2023-01-18 20:19:59 -0700

    Final commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "WISE Home Solutions";
+        job1._jobTitle = "Director of Call Center Operations";
+        job1._startYear = "2018";
+        job1._endYear = "Present";
+
+        Job job2 = new Job();
+        job2._company = "Landmark Home Warranty";
+        job2._jobTitle = "Director Inside Sales";
+        job2._startYear = "2012";
+        job2._endYear = "2017";
+
+        Resume myResume = new Resume();
+        myResume._name = "David Mendenhall";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();       
     }
 }
